5fd364c7ef6233174452ce7ce4ee0e8b9df940e2
stephen kinyori <kinyorimorris@gmail.com>
2019-06-04 21:18:23 +0300

    remove unique attribute from user role column

--- models/user.js
+++ models/user.js
@@ -1,22 +1,21 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const user = sequelize.define('user', {
-    username: {
-      type: DataTypes.STRING,
-      allowNull:false,
-      unique: true
-    },
-    role: {
-      type: DataTypes.ENUM({
-        values: ['Retailer', 'WareHouseAttendant']
-      }),
-      allowNull:false,
-      unique: true
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull:false
-    }
-  }, {});
-  return user;
+    const user = sequelize.define('user', {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        role: {
+            type: DataTypes.ENUM({
+                values: ['Retailer', 'WareHouseAttendant']
+            }),
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {});
+    return user;
 };

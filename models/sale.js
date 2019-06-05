a6285aab9fd62078e61873e34612495b52916e0d
stephen kinyori <kinyorimorris@gmail.com>
2019-06-05 09:16:08 +0300

    added after create sale action

--- models/sale.js
+++ models/sale.js
@@ -1,4 +1,5 @@
 'use strict';
+const Inv = require('models');
 module.exports = (sequelize, DataTypes) => {
     const sale = sequelize.define('sale', {
         quantity: DataTypes.INTEGER
@@ -7,5 +8,14 @@ module.exports = (sequelize, DataTypes) => {
         sale.belongsTo(product);
         sale.belongsTo(inventory)
     };
+    /*
+        Reduce the stock quantity of the oldest batch
+     */
+    sale.afterCreate((sale) => {
+        return sale.getInventory(inventory => {
+            //@todo test this and verify that records are retured from association
+            console.log(inventory);
+        })
+    });
     return sale;
 };

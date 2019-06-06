6c67c813bc1f9f901d9e9bfd33e7a5ac1d969fc7
stephen kinyori <kinyorimorris@gmail.com>
2019-06-06 12:09:42 +0300

    Create sale for earliest batch

--- controllers/sales.js
+++ controllers/sales.js
@@ -4,14 +4,31 @@ const db = require('./../models');
 //@todo ensure sale reduces the oldest inventory with available stock;
 module.exports = {
     create(req, res) {
-        return Sale.create(
+        return db.inventory.findAll(
             {
-                quantity: req.body.quantity,
-                productId: req.body.productId,
-                inventoryId: req.body.inventoryId
-            })
-            .then(sale => res.status(201).send(sale))
-            .catch(error => res.status(401).send(error))
+                where: {
+                    stockQuantity: {
+                        [db.Sequelize.Op.gte]: 0
+                    },
+                    productId: req.body.productId
+                },
+                include: [{
+                    model: db.product
+                }],
+                group: ['id'],
+                order: [['createdAt', 'DESC']],
+                limit: 1
+            }
+        ).then(batch => {
+            return Sale.create(
+                {
+                    quantity: req.body.quantity,
+                    productId: req.body.productId,
+                    inventoryId: batch[0].id
+                })
+                .then(sale => res.status(201).send(sale))
+                .catch(error => res.status(401).send(error))
+        });
     },
     fetchAll(req, res) {
         return Sale.findAll({
